9e5f2d154cca6d12b7ec9f5b5fd5a74af9c39cf7
brandong97 <116442100+brandong97@users.noreply.github.com>
2025-03-03 08:23:55 +0000

    The home page is set to something we can pull up

--- config/routes.rb
+++ config/routes.rb
@@ -45,4 +45,14 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+<<<<<<< HEAD
+=======
+
+  # Defines the root path route ("/")
+<<<<<<< HEAD
+>>>>>>> ca9b9fa (The home page is set to something we can pull up)
  end
+=======
+  root 'home#index'
+end
+>>>>>>> c87cf15 (The home page is set to something we can pull up)
