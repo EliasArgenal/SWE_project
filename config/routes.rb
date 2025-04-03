fbdf33ea6c6e1e57a74cf637b9932bb80ec57aee
brandong97 <116442100+brandong97@users.noreply.github.com>
2025-04-03 05:30:59 +0000

    Dashboard added, fixed login issue, routes rearranged for clarity

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,31 @@
 Rails.application.routes.draw do
+  # Home Page
+  get '/home', to: 'home#index', as: 'home'
+  root 'home#index'
+
+   # Login routes
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  get "/logout", to: "sessions#destroy", as: '/logout'
+  # Signup route
+  get '/signup', to: 'users#new', as: 'signup'
+  post '/signup', to: 'users#create'
+
+  resources :users, only: [:new, :create]
+
+   # Lessons routes
+  resources :lessons, only: [:index, :show] do
+    resources :content_pages, only: [:show] # Nested content pages under lessons
+    resources :assessment_pages, only: [:show] # Should only be one assesment page in lesson
+  end
+  
+  # Forum Routes
+  get 'forum/general_discussion', to: 'forum_posts#index', defaults: { category: 'general' }, as: 'general_discussion'
+  get 'forum/help_support', to: 'forum_posts#index', defaults: { category: 'help' }, as: 'help_support'
+  get 'forum/off_topic', to: 'forum_posts#index', defaults: { category: 'offtopic' }, as: 'off_topic'
+
+  resources :forum_posts
+
   get "assessment_pages/show"
   get "forum_posts/index"
   get "forum_posts/show"
@@ -7,32 +34,6 @@ Rails.application.routes.draw do
   get "content_pages/show"
   get "lessons/index"
   get "lessons/show"
-  # signup route
-  get '/signup', to: 'users#new', as: 'signup'
-  # Root route (default to login page)
- 
-  # Login routes
-  get '/login', to: 'sessions#new', as: 'login'
-  post '/login', to: 'sessions#create'
-  get "/logout", to: "sessions#destroy", as: '/logout'
-
-  # Home Page
-  get '/home', to: 'home#index', as: 'home'
-  root 'home#index'
-
-  # Lessons routes
-  resources :lessons, only: [:index, :show] do
-    resources :content_pages, only: [:show] # Nested content pages under lessons
-    resources :assessment_pages, only: [:show] # Should only be one assesment page in lesson
-  end
-
-
-# Forum Routes
-get 'forum/general_discussion', to: 'forum_posts#index', defaults: { category: 'general' }, as: 'general_discussion'
-get 'forum/help_support', to: 'forum_posts#index', defaults: { category: 'help' }, as: 'help_support'
-get 'forum/off_topic', to: 'forum_posts#index', defaults: { category: 'offtopic' }, as: 'off_topic'
-
-resources :forum_posts
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -44,6 +45,4 @@ resources :forum_posts
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
  end
